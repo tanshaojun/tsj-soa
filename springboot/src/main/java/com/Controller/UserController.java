ed5d485f644b38e1567ccc63d7aee8d579fdd9ce
tsj <767650993@qq.com>
2018-04-27 17:51:57 +0800

    m

--- springboot/src/main/java/com/Controller/UserController.java
+++ springboot/src/main/java/com/Controller/UserController.java
@@ -3,14 +3,23 @@ package com.Controller;
 import com.bean.User;
 import com.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class UserController {
     @Autowired
     private UserService userService;
 
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @ResponseBody
     @RequestMapping("/login")
     public String login() {
         System.out.println(userService);
