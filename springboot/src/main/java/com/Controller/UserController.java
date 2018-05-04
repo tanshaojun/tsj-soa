5c70d2c75041599af2b14f0f27d0fa25152cc951
tsj <767650993@qq.com>
2018-05-04 17:55:54 +0800

    m

--- springboot/src/main/java/com/Controller/UserController.java
+++ springboot/src/main/java/com/Controller/UserController.java
@@ -13,7 +13,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/")
+    @RequestMapping("/index")
     public String index() {
         return "index";
     }
---
+++ springboot/src/main/java/com/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+    @RequestMapping(value = "/")
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView("index1");
+        return mv;
+    }
+}
