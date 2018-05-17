f9f4217c76ba06b3d30920a013a82b359a43ee31
tsj <767650993@qq.com>
2018-05-17 19:56:33 +0800

    m

--- springboot/src/main/java/com/SpringMainApplication.java
+++ springboot/src/main/java/com/SpringMainApplication.java
@@ -1,6 +1,7 @@
 package com;
 
 
+import com.utils.HttpUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
