56b7a050cf70756accca100222419b2f187bebe7
tsj <767650993@qq.com>
2018-07-13 19:26:05 +0800

    m

---
+++ springboot/src/main/java/com/demo/checkPossibility.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+public class checkPossibility {
+    public static boolean checkPossibility(int[] nums) {
+        int index = 0;
+        int index1 = nums.length - 1;
+        while (index == index1) {
+
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ springboot/src/main/java/com/demo/missingNumber.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+public class missingNumber {
+    public static int missingNumber(int[] nums) {
+        int a = 0;
+        int b = 0;
+        for (int i = 0; i < nums.length; i++) {
+            a += i;
+            b += nums[i];
+        }
+        return a + nums.length - b;
+    }
+
+    public static void main(String[] args) {
+        int a = 8;
+        System.out.println(missingNumber(new int[]{9,6,4,2,3,5,7,0,1}));
+    }
+}
