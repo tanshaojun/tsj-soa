30a66abf4775e6e8bb285ed3a96b8cd9fa79f31d
tsj <tsj@qq.com>
2018-08-01 21:10:55 +0800

    m

---
+++ springboot-el/src/main/java/com/SpringBootELApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created with IntelliJ IDEA
+ * Created By Kinsey
+ * Date: 2018/8/1
+ * Time: 下午9:06
+ */
+@SpringBootApplication
+public class SpringBootELApplication {
+}
