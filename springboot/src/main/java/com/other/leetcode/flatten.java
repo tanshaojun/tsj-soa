11b9480b9dee8f2d46577b568719138ca73032c5
tsj <767650993@qq.com>
2019-06-26 15:21:29 +0800

    m

---
+++ springboot/src/main/java/com/other/leetcode/flatten.java
@@ -0,0 +1,39 @@
+package com.other.leetcode;
+
+import java.util.Stack;
+
+/**
+ * @author tanshaojun
+ * @version 1.0
+ * @date 2019/6/2016:58
+ */
+public class flatten {
+
+    public static void flatten(TreeNode root) {
+        if (null == root) return;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode head = new TreeNode(0);
+        TreeNode result = head;
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode pop = stack.pop();
+            head.right = pop;
+            head.left = null;
+            head = head.right;
+            if (pop.right != null) stack.push(pop.right);
+            if (pop.left != null) stack.push(pop.left);
+        }
+        root = result.right;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+//        root.left.left = new TreeNode(3);
+//        root.left.right = new TreeNode(4);
+//        root.right = new TreeNode(5);
+//        root.right.right = new TreeNode(6);
+
+        flatten(root);
+    }
+}
